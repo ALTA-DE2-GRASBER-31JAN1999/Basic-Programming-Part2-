4fe3ff089a9d277bd48b5177a11409817be07f9e
Asberg <nahumarurygrasberg@gmail.com>
2024-02-20 21:30:58 +0700

    solve problem 2

--- problem2/main.py
+++ problem2/main.py
@@ -1,6 +1,8 @@
 # input
+rumus = 0.5
 alas = 20.0
 tinggi = 25.0
 
 
 # kode disini
+print(rumus * alas * tinggi)
--- problem3/main.py
+++ problem3/main.py
@@ -4,3 +4,6 @@ r = 4.0
 
 
 # kode disini
+luas_permukaan = 2 * 3.14 * r * (r + T)
+
+print(luas_permukaan)
